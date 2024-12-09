5b5da41f857951d22a989d75d2a5429cd75fe2a7
Krisz-banki <77264324+Krisz-banki@users.noreply.github.com>
2024-12-09 12:55:02 +0100

    Új receptűrlap vizuális kialakítása

---
+++ src/components/RecipeForm.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+    <form>
+        <div class="mb-3">
+            <label for="formFile" class="form-label">Töltsön fel képet az ételről!</label>
+            <input class="form-control" type="file" id="formFile">
+        </div>
+        <input class="form-control" type="text" placeholder="Étel neve">
+        <input class="form-control" type="number" placeholder="Elkészítési idő">
+        <input class="form-control" type="text" placeholder="Elkészítés nehézsége">
+        <button type="button" class="btn btn-success">Recept hozzáadása</button>
+    </form>
+</template>
+
+<style scoped>
+
+</style>
