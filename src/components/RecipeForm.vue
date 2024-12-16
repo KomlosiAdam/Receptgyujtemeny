a53eb186c7cdf99edb55b26d44cc8f5c1887e1a3
Krisz-banki <77264324+Krisz-banki@users.noreply.github.com>
2024-12-16 09:52:02 +0100

    Kép feltöltéses opció törlése

--- src/components/RecipeForm.vue
+++ src/components/RecipeForm.vue
@@ -4,10 +4,6 @@
 
 <template>
     <form>
-        <div class="mb-3">
-            <label for="formFile" class="form-label">Töltsön fel képet az ételről!</label>
-            <input class="form-control" type="file" id="formFile">
-        </div>
         <input class="form-control" type="text" placeholder="Étel neve">
         <input class="form-control" type="number" placeholder="Elkészítési idő">
         <input class="form-control" type="text" placeholder="Elkészítés nehézsége">
